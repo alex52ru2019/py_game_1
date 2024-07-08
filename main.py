fd9b4282985712da48864683e8f1cec9c5821a93
alex52ru2019 <57319391+alex52ru2019@users.noreply.github.com>
2024-07-08 16:08:05 +0300

    добавил комментарии

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ pygame.init()
 
 SCREEN_WIDTH = 800  #ширина окна
 SCREEN_HEIGHT = 600  #высота окна
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #установки окна для игры
 
-pygame.display.set_caption("Игра в Тир")
-icon = pygame.image.load("тир.jpg")
-pygame.display.set_icon(icon)
+pygame.display.set_caption("Игра в Тир") #название окна
+icon = pygame.image.load("тир.jpg") #загружаем иконку
+pygame.display.set_icon(icon) #устанавливаем иконку
 
-targen_img = pygame.image.load("img/цель_80.png")
-target_widtg = 80
-target_height = 80
+targen_img = pygame.image.load("img/цель_80.png") #загружаем мишень
+target_widtg = 80 #ширина мишени
+target_height = 80 #высота мишени
 
-target_x = random.randint(0, SCREEN_WIDTH-target_widtg)
-target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+target_x = random.randint(0, SCREEN_WIDTH-target_widtg) #координата х мишени
+target_y = random.randint(0, SCREEN_HEIGHT-target_height) #координата у мишени
 
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) #цвет фона
 
 
 
