9ae3e77c7cdd487397ae6ee7c0fd50395f397f06
alex52ru2019 <57319391+alex52ru2019@users.noreply.github.com>
2024-07-08 15:41:13 +0300

    add all const

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -7,6 +9,19 @@ SCREEN_HEIGHT = 600  #высота окна
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра в Тир")
+icon = pygame.image.load("тир.jpg")
+pygame.display.set_icon(icon)
+
+targen_img = pygame.image.load("img/цель_80.png")
+target_widtg = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH-target_widtg)
+target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
 
 running = True #переменная для цикла
 while running: #цикл игры
